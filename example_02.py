c8c3c3d99f50e8d1b7e8978cf7e171125813312c
Rokon Ron <rokon.raz@gmail.com>
2023-07-27 23:19:18 +0600

    testing for some element and here using list comprehension.

--- example_02.py
+++ example_02.py
@@ -1,23 +1,37 @@
-from datetime import datetime, timedelta
+# from datetime import datetime, timedelta
+#
+# user_input = input("When would you like to travel (YYYY-MM-DD format): ")
+#
+# try:
+#     # Convert user input to a datetime object
+#     travel_date = datetime.strptime(user_input, '%Y-%m-%d')
+#
+#     # Create a list to store the dates
+#     date_list = []
+#
+#     # Add the first date to the list
+#     date_list.append(travel_date.strftime('%Y-%m-%d'))
+#
+#     # Generate the next 9 dates and add them to the list
+#     for i in range(9):
+#         travel_date += timedelta(days=1)
+#         date_list.append(travel_date.strftime('%Y-%m-%d'))
+#
+#     print(date_list)
+#
+# except ValueError:
+#     print("Invalid date format. Please use the YYYY-MM-DD format.")
 
-user_input = input("When would you like to travel (YYYY-MM-DD format): ")
 
-try:
-    # Convert user input to a datetime object
-    travel_date = datetime.strptime(user_input, '%Y-%m-%d')
+from bs4 import BeautifulSoup
+import requests
 
-    # Create a list to store the dates
-    date_list = []
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
 
-    # Add the first date to the list
-    date_list.append(travel_date.strftime('%Y-%m-%d'))
+response = requests.get("https://www.billboard.com/charts/hot-100/" + date)
 
-    # Generate the next 9 dates and add them to the list
-    for i in range(9):
-        travel_date += timedelta(days=1)
-        date_list.append(travel_date.strftime('%Y-%m-%d'))
+soup = BeautifulSoup(response.text, 'html.parser')
+song_names_spans = soup.select("li ul li h3")
 
-    print(date_list)
-
-except ValueError:
-    print("Invalid date format. Please use the YYYY-MM-DD format.")
+song_names = [song.getText().strip() for song in song_names_spans]
+print(song_names)
