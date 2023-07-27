e2918f02144e16c3a661733be73aced8fe26fa1e
Rokon Ron <rokon.raz@gmail.com>
2023-07-27 23:17:22 +0600

    Here use bs4 in select() method for searching specific element

--- main.py
+++ main.py
@@ -15,13 +15,20 @@ except ValueError:
 url = "https://www.billboard.com/charts/hot-100/2000-08-12/"
 
 url_request = requests.get(url)
+url_request.raise_for_status()
 
 url_request_text = url_request.text
 
 
 soup = BeautifulSoup(url_request_text, "html.parser")
 
+articel_song_list = soup.select("li ul li h3")
 
+for tag in articel_song_list:
+    article_song_list_text = tag.getText()
+
+    print(article_song_list_text.strip())
+# print(soup.text)
 
 
 
